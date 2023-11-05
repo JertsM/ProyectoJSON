e636c3df5c88d8c2b6fb34d40613a14bca712b31
JertsM <mauricioeme46@outlook.com>
2023-11-05 21:35:38 +0100

    AvanceJSON

--- src/model/Principal.java
+++ src/model/Principal.java
@@ -1,13 +1,10 @@
 package model;
 
-import java.io.IOException;
-
 public class Principal {
 
 	public static void main(String[] args){
 		AplicacionUsuarios u1 = new AplicacionUsuarios();
 		u1.crearFicheroJson();
-		u1.crearUsuario("", "", "", "");
 		u1.ejecutar();
 	}
 }
